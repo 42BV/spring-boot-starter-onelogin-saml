4138c57eae98ca8e3e6c7ccad7f30c98b1e02f2c
Jeroen van Schagen <jeroen.van.schagen@42.nl>
2023-05-25 14:20:50 +0200

    SAML registrations without SP_ID now default to the login URL

--- src/test/java/nl/_42/boot/onelogin/saml/Saml2OneLoginAutoConfigurationTest.java
+++ src/test/java/nl/_42/boot/onelogin/saml/Saml2OneLoginAutoConfigurationTest.java
@@ -1,11 +1,19 @@
 package nl._42.boot.onelogin.saml;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 class Saml2OneLoginAutoConfigurationTest extends AbstractSpringBootTest {
 
+	@Autowired
+	private Saml2Properties properties;
+
 	@Test
 	void configuration_shouldSucceed() {
+		Registration registration = properties.getRegistration("saml-service");
+		Assertions.assertEquals("https://myservice/saml2/login/saml-service", registration.getServiceProviderId());
+		Assertions.assertEquals("https://saml-service/sso", registration.getIdentityProviderId());
 	}
 
 }
