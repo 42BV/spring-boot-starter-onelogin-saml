1887d037e6b3870a4396afc6d4ec72a3d1e5a3f1
Jeroen van Schagen <jeroen.van.schagen@42.nl>
2023-09-12 09:39:16 +0200

    Added context refresh listener to update the SAML2 properties

--- src/test/java/nl/_42/boot/onelogin/saml/Saml2OneLoginAutoConfigurationTest.java
+++ src/test/java/nl/_42/boot/onelogin/saml/Saml2OneLoginAutoConfigurationTest.java
@@ -3,17 +3,35 @@ package nl._42.boot.onelogin.saml;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.endpoint.event.RefreshEvent;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.event.ContextRefreshedEvent;
 
 class Saml2OneLoginAutoConfigurationTest extends AbstractSpringBootTest {
 
 	@Autowired
 	private Saml2Properties properties;
 
+	@Autowired
+	private ApplicationEventPublisher publisher;
+
+	@Autowired
+	private ApplicationContext context;
+
 	@Test
 	void configuration_shouldSucceed() {
 		Registration registration = properties.getRegistration("saml-service");
 		Assertions.assertEquals("https://myservice/saml2/login/saml-service", registration.getServiceProviderId());
 		Assertions.assertEquals("https://saml-service/sso", registration.getIdentityProviderId());
+	}
+
+	@Test
+	void refresh_shouldSucceed() {
+		publisher.publishEvent(new RefreshEvent(context, this, "Test"));
+
+		Registration registration = properties.getRegistration("saml-service");
+		Assertions.assertEquals("https://myservice/saml2/login/saml-service", registration.getServiceProviderId());
 	}
 
 }
