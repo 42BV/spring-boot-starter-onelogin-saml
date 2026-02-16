39d5986214044e1a0d0d90027e6178842b9a8c07
Michel Greve <michel.greve@42.nl>
2026-02-16 13:43:08 +0100

    SEED-154, Put back some dependencies and the Saml2RefreshEventListener

---
+++ src/main/java/nl/_42/boot/onelogin/saml/Saml2RefreshEventListener.java
@@ -0,0 +1,32 @@
+package nl._42.boot.onelogin.saml;
+
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@ConditionalOnClass(name = "org.springframework.cloud.endpoint.event.RefreshEvent")
+public class Saml2RefreshEventListener {
+
+    private final ApplicationContext applicationContext;
+    private final Saml2Properties properties;
+
+    public Saml2RefreshEventListener(ApplicationContext applicationContext, Saml2Properties properties) {
+        this.applicationContext = applicationContext;
+        this.properties = properties;
+    }
+
+    @EventListener
+    public void handleContextRefresh(org.springframework.cloud.endpoint.event.RefreshEvent event) {
+        log.info("Refreshing SAML2 properties after event '{}'...", event.getEvent());
+        AutowireCapableBeanFactory beanFactory = applicationContext.getAutowireCapableBeanFactory();
+        Saml2Properties refreshed = beanFactory.createBean(Saml2Properties.class);
+        properties.refresh(refreshed);
+    }
+
+}
